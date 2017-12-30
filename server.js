12ec4e52712ff4aebf7880de47537320886b11e5
Luciana Castro <lucianaycastro@gmail.com>
2017-12-30 11:43:43 -0300

    api updates

--- server.js
+++ server.js
@@ -2,10 +2,33 @@ console.log('hola');
 
 var express = require('express');
 var app = express();
+var cities = require('./cities.json').cities;
+
+cities.forEach(function(city) {
+	city._link = 'http://localhost:3000/api/cities/' + escape(city.name);
+});
 
 app.get('/', function(req, res) {
-	console.log('Me pidieron hello world');
+	console.log('Me pidieron hello world, para ', req.query);
 	res.send('hello world');
+});
+
+app.get('/api/cities', function(req, res) {
+	res.send({
+		items: cities
+	});
+});
+
+app.get('/api/cities/:cityName', function(req, res) {
+	var cityName = req.params.cityName;
+	var city = cities.find(function(city){
+		return city.name === cityName;
+	});
+	if (city) {
+		res.send(city);	
+	} else {
+		res.status(404).send({ error: 'not found' });
+	}
 });
 
 app.listen(3000, function() {
